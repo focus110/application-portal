d4de835fdec46a41264c28c01143a24556879956
focus110 <iamemmaochuko@gmail.com>
2022-05-05 23:59:20 +0100

    setup login and register route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import React from 'react'
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import LoginPage from './components/pages/LoginPage'
+import RegisterPage from './components/pages/RegisterPage'
 
 function App() {
   return (
-    <div className="App">
-        <p>
-         Application portal up and running
-        </p>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={ <LoginPage /> } />
+        <Route path="/" element={ <RegisterPage /> } />
+      </Routes>
+    </Router>
   );
 }
 
