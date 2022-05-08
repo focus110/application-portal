e7f007e023fe071be0164d5cd75ca341873d0c5e
focus110 <iamemmaochuko@gmail.com>
2022-05-08 13:01:47 +0100

    Login & registration frontend compeleted

--- src/components/pages/RegisterPage.js
+++ src/components/pages/RegisterPage.js
@@ -1,128 +1,183 @@
 import React from "react";
-import background from "../img/frame1.png";
+import background from "../../img/frame1.png";
+import { Link } from "react-router-dom";
+import logo from "../../img/logo.png";
 
 const RegisterPage = () => {
   return (
-    <div className="min-h-screen w-full flex justify-center lg:justify-start items-center">
-      <div
-        style={{
-          backgroundImage: `url(${background})`,
-        }}
-        className="bg-rectem-50 w-6/12 h-screen hidden lg:block"
-      >
-        <div className="text-3xl text-white p-24 absolute left-0 bottom-0">
-          Redeemers College of Science and <br /> Technology
-        </div>
+    <div className="2xl:container h-screen m-auto">
+      <div hidden className="fixed inset-0 w-6/12 lg:block">
+        <img className="w-full h-full object-cover" src={background} alt="" />
       </div>
-      <div className="w-full lg:w-5/12 flex-1 max-w-xs lg:ml-28">
-        <div>
-          <div className="bg-white rounded w-full mb-6">
-            <h1 className="text-lg text-gray-900 font-bold mt-16 mb-24">
+      <div className="relative h-full ml-auto lg:w-6/12">
+        <div className="mx-auto py-12 px-6 sm:p-20 xl:w-10/12">
+          <div className="space-y-2">
+            <Link to="/register">
+              <img src={logo} className="w-40" alt="rectem logo" />
+            </Link>
+            <h1 className="text-2xl text-rectem-black font-bold mt-16 mb-24">
               Register
             </h1>
-
-            <form>
-              <form className="mb-4">
-                <label className="block text-sm text-gray-900 mb-2">
-                  Firstname*
-                </label>
-                <input
-                  id="email"
-                  type="email"
-                  className="block w-full rounded-3xl border bg-white py-2 px-3 text-sm"
-                  name="email"
-                  placeholder="firstname"
-                  required
-                  autofocus
-                />
-              </form>
-
-              <form className="mb-4">
-                <label className="block text-sm text-gray-900 mb-2">
-                  Lastname*
-                </label>
-                <input
-                  id="email"
-                  type="email"
-                  className="block w-full rounded-3xl border bg-white py-2 px-3 text-sm"
-                  name="email"
-                  placeholder="lastname"
-                  required
-                  autofocus
-                />
-              </form>
-
-              <form className="mb-4">
-                <label className="block text-sm text-gray-900 mb-2">
-                  Username*
-                </label>
-                <input
-                  id="email"
-                  type="email"
-                  className="block w-full rounded-3xl border bg-white py-2 px-3 text-sm"
-                  name="email"
-                  placeholder="username"
-                  required
-                  autofocus
-                />
-              </form>
-
-              <form className="mb-4">
-                <div className="w-full flex justify-between items-center">
-                  <label
-                    for="password"
-                    className="block text-sm text-gray-900 mb-2"
-                  >
-                    email*
-                  </label>
-                </div>
-                <input
-                  id="password"
-                  type="password"
-                  className="block w-full rounded-3xl border bg-white py-2 px-3 text-sm"
-                  name="password"
-                  placeholder="email"
-                  required
-                />
-              </form>
-
-              <div className="flex justify-between pt-1 pb-5 text-sm text-gray-darker font-thin">
-                <label>
-                  <input
-                    className="mr-1"
-                    type="checkbox"
-                    name="remember"
-                    id="remember"
-                  />{" "}
-                  Remember me
-                </label>
-                <a
-                  className="text-xs font-thin text-blue no-underline hover:underline"
-                  href="#"
-                >
-                  Forgotten password?
-                </a>
-              </div>
-
-              <a
-                href="/"
-                className="block w-full text-center bg-rectem-50 text-white rounded-3xl py-3 text-sm tracking-wide"
-              >
-                Sign in
-              </a>
-            </form>
+            <p className="font-medium text-sm text-rectem-grey">
+              Start your admission process
+            </p>
           </div>
 
-          <p className="text-center lg:text-left text-sm text-gray-600 font-thin mb-32">
-            Already have an account yet?{" "}
-            <a href="/" class="text-rectem-50 no-underline hover:underline">
-              Sign in
-            </a>
-          </p>
+          <form action="" className="space-y-6 py-6">
+            <div className="flex w-full space-x-4">
+              <div className="w-full">
+                <label className="block text-sm font-medium text-rectem-100 mb-2">
+                  Firstname
+                </label>
+                <input
+                  type="text"
+                  className="block w-full rounded-3xl border bg-white py-2.5 px-5 text-sm text-rectem-grey outline-none focus:border-rectem-50"
+                  placeholder="email"
+                />
+              </div>
+              <div className="w-full">
+                <label className="block text-sm font-medium text-rectem-100 mb-2">
+                  Lastname
+                </label>
+                <input
+                  type="text"
+                  className="block w-full rounded-3xl border bg-white py-2.5 px-5 text-sm text-rectem-grey outline-none focus:border-rectem-50"
+                  placeholder="lastname"
+                />
+              </div>
+            </div>
 
-          <p className="text-center lg:text-left text-sm text-gray-600 font-thin">
-            2022 All right reserved
-          </p>
+            <div className="flex w-full space-x-4">
+              <div className="w-full">
+                <label className="block text-sm font-medium text-rectem-100 mb-2">
+                  Username
+                </label>
+                <input
+                  type="text"
+                  className="block w-full rounded-3xl border bg-white py-2.5 px-5 text-sm text-rectem-grey outline-none focus:border-rectem-50"
+                  placeholder="username"
+                />
+              </div>
+              <div className="w-full">
+                <label className="block text-sm font-medium text-rectem-100 mb-2">
+                  Sex
+                </label>
+                <div className="w-full relative">
+                  <select
+                    class="appearance-none block w-full px-5 py-2.5 text-sm font-normal text-rectem-grey bg-white bg-clip-padding bg-no-repeat border rounded-3xl transition ease-in-out
+      m-0
+      focus:text-rectem-grey focus:bg-white focus:border-rectem-50 focus:outline-none"
+                    aria-label="Default select example"
+                  >
+                    <option value="male">choose your gender</option>
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                  </select>
+                  <div className="absolute top-4 right-4 w-6 text-rectem-grey">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="h-5 w-5"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                        clip-rule="evenodd"
+                      />
+                    </svg>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex w-full space-x-4">
+              <div className="w-full">
+                <label className="block text-sm font-medium text-rectem-100 mb-2">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  className="block w-full rounded-3xl border bg-white py-2.5 px-5 text-sm text-rectem-grey outline-none focus:border-rectem-50"
+                  placeholder="email"
+                />
+              </div>
+              <div className="w-full">
+                <label className="block text-sm font-medium text-rectem-100 mb-2">
+                  Phone
+                </label>
+                <input
+                  type="number"
+                  className="block w-full rounded-3xl border bg-white py-2.5 px-5 text-sm text-rectem-grey outline-none focus:border-rectem-50"
+                  pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
+                  placeholder="phone"
+                />
+              </div>
+            </div>
+
+            <div className="flex flex-col items-start">
+              <label className="block text-sm font-medium text-rectem-100 mb-2">
+                Password
+              </label>
+              <input
+                type="password"
+                className="block w-full rounded-3xl border bg-white py-2.5 px-5 text-sm text-rectem-grey outline-none focus:border-rectem-50"
+                placeholder="password"
+              />
+            </div>
+
+            <div className="flex items-center text-sm font-medium text-rectem-100 cursor-pointer relative">
+              <input
+                className="mr-2 text-rectem-50 text-2xl appearance-none rounded border border-rectem-50 h-5 w-5"
+                type="checkbox"
+                name="remember"
+                id="remember"
+              />
+              <div className="absolute text-rectem-50">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-5 w-5"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+              </div>
+              <div>
+                I agree to the{" "}
+                <span className="text-rectem-50">Tearms & Conditions</span>
+              </div>
+            </div>
+
+            <div className="w-full">
+              <button className="w-full px-6 py-3 block text-center bg-rectem-50 text-white rounded-3xl text-sm transition">
+                <span className="font-semibold text-white text-lg">
+                  Sign up
+                </span>
+              </button>
+              <Link
+                to="/"
+                className="text-sm tracking-wide font-medium text-rectem-100"
+              >
+                Already have an account?{" "}
+                <span class="text-rectem-50 no-underline hover:underline">
+                  Sign in
+                </span>
+              </Link>
+            </div>
+          </form>
+
+          {/* <div className="pt-12">
+            <div className="space-y-2 text-left">
+              <span className="block text-sm tracking-wide text-gray-500">
+                2022 All right reserved
+              </span>
+            </div>
+          </div> */}
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,17 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        display: ["montserrat"],
+        body: ["poppins"],
+      },
       colors: {
         rectem: {
           50: "#23268C",
+          100: "#4C4A55",
+          grey: "#8C8B93",
+          form: "#F3F3F3",
+          black: "#0F0D1A",
         },
       },
     },
